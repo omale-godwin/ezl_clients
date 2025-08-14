f5627d4b4968b4226d01a81731f548f17f94c0a2
omale-godwin <godwinpeace3@gmail.com>
2025-08-14 17:42:02 +0100

    ip

--- wordpress-ezl/wp-content/plugins/wavy-functions/theme-functions/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once(EPCL_PLUGIN_PATH.'/theme-functions/enqueue-scripts.php');    
-require_once(EPCL_PLUGIN_PATH.'/theme-functions/render-ads.php');          
-require_once(EPCL_PLUGIN_PATH.'/theme-functions/custom-styles.php');  
-require_once(EPCL_PLUGIN_PATH.'/theme-functions/social-buttons.php');    
-require_once(EPCL_PLUGIN_PATH.'/theme-functions/theme-functions.php');
